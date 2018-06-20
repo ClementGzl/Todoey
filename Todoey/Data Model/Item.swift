e4e0358e8aaf9b2be76628702603a4895d57f2f3
ClementGzl <clement@gonzalvez.com>
2018-06-20 15:12:14 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  ItemArray.swift
+//  Todoey
+//
+//  Created by Clément on 20/06/2018.
+//  Copyright © 2018 Clément. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done = false
+
+}
